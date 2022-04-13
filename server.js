fa0e51a203b29960e585e6728c91e33cd25d0324
RAVI SHARMA <ravics09@RAVIs-MacBook-Air.local>
2022-04-13 15:12:32 +0530

    second commit to correct path of index.html

--- server.js
+++ server.js
@@ -17,11 +17,17 @@ app.use(helmet.hidePoweredBy());
 app.use(morgan("tiny"));
 app.use(express.json());
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.resolve(__dirname, "./client/build")));
+// if(process.env.NODE_ENV === "production"){
+//     app.use(express.static(path.resolve(__dirname, "./client/build")));
 
-}
+// }
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.get("*", function (request, response) {
+    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+  });
+  
 app.use('/user', userRoute);
+
 
 app.listen(PORT, (err, res)=>{
     if(err) return err;
