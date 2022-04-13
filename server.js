cf3e46b65a046e5a570e1d0c86638e569c823302
RAVI SHARMA <ravics09@RAVIs-MacBook-Air.local>
2022-04-13 16:04:44 +0530

    modified mongodb atlas uri l

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const express = require("express");
 
 const connectDB = require("./config/connectDB");
 const userRoute = require("./routers/userRoute");
-const PORT = process.env.NODE_ENV === "production" ? process.env.PORT || 80 : 9090;
+const PORT = process.env.NODE_ENV === "production" ? process.env.PORT : 9090;
 
 connectDB();
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 // app.use(helmet.hidePoweredBy());
 app.use(morgan("tiny"));
 app.use(express.json());
